a7533d722a2a21a030763a3c24aa1e8209989a50
Muhammad Tayyab <mtbahaudin@taifco.net>
2023-05-13 14:33:49 +0500

    + Shader Painter

---
+++ lib/common/shader_painter.dart
@@ -0,0 +1,21 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class ShaderPainter extends CustomPainter {
+  ShaderPainter(this.shader, {this.update});
+
+  final FragmentShader shader;
+  final void Function(FragmentShader, Size)? update;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    update?.call(shader, size);
+    canvas.drawRect(
+        Rect.fromLTWH(0, 0, size.width, size.height), Paint()..shader = shader);
+  }
+
+  @override
+  bool shouldRepaint(covariant ShaderPainter oldDelegate) {
+    return oldDelegate.shader != shader || oldDelegate.update != update;
+  }
+}
